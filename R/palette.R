61f19a6b6cf58380576b53896f77a394ff2afd0d
Shinya Uryu <suika1127@gmail.com>
2015-05-16 08:07:48 +0900

    [ADD]initial commit

---
+++ R/palette.R
@@ -0,0 +1,17 @@
+#' Precure all star's color palette
+#'
+#' @export
+precure_pal <- list(
+  PretyCure = c("#060825", "#FEFEFE"),
+  MaxHeart = c("#1E1624", "#F5F2F6", "#F6F15B"),
+  SplashStar = c("#EE3185", "#4F225D"),
+  YesPreCure5 = c("#E02C85", "#CA4241", "#1F5CAF", "#0F8355", "#F8EB5D"),
+  YesPreCure5GoGo = c("#E62581", "#DE3228", "#F5E339", "#048161", "#133A99", "#AF51E7"),
+  Fresh           = c("#F9D1DE", "#357CC2", "#FDD549", "#B6203B"),
+  HeartCatch      = c("#F250B4", "#67D6EC", "#F9E35D", "#CEC5D8"),
+  Suite           = c("#D23B87", "#F4ED8C", "#7390ED", "#F0C32D"),
+  Smile           = c("#A61F68", "#E7350D", "#F9E04A", "#51CF51", "#7CA2F5"),
+  DokiDoki        = c("#F578CB", "#595A83", "#F3A443", "#D7A5F3"),
+  HappinessCharge = c("#D02A8A", "#74A2EA", "#F4C542", "#9689EA"),
+  Princess        = c("#EA91C3", "#A3E5E7", "#E78717")
+)
---
+++ R/precure.R
@@ -0,0 +1,5 @@
+#' @title Pretty Cure Color Palette for R
+#' @name precure
+#' @docType package
+#' @details Palettes provide from Pretty Cure series that Japanese animation
+NULL
